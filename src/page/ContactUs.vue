7d1fd4746cc6436021253bc43dd5aaa222cc7853
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-07-16 18:10:52 +0200

    feat: add base markup

---
+++ src/page/ContactUs.vue
@@ -0,0 +1,19 @@
+
+<script>
+export default {
+    name:'ContactUs'
+    
+}
+</script>
+
+<template>
+    <h1>Parte Contact Us</h1>
+    
+</template>
+
+<style lang="scss">
+@use '../style/generals.scss' as *;
+@use '../style/partials/mixins' as *;
+@use '../style/partials/variables' as *;
+    
+</style>
