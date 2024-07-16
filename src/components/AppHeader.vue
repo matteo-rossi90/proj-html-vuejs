8eaf1cdc8e6dffd30af66e96cf86b7e10680f611
battistaennio <ennio.battista99@gmail.com>
2024-07-16 18:09:16 +0200

    feat: add header top banner

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,12 +3,46 @@ export default {
     name:'AppHeader'
 }
 </script>
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+    <header>
+        <section id="top-banner">
+            <div class="container">
+                <div id="updates">news updates</div>
+
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+    
+            </div>
+        </section>
+    </header>
 </template>
 
-<style lang="">
-    
+<style lang="scss" scoped>
+    @use '../style/partials/variables' as *;
+
+    .container{
+        margin: 0 10%;
+
+        #updates{
+            text-transform: uppercase;
+            padding: 10px;
+            background-color: #545454;
+            color: white;
+        }
+
+        #social{
+
+        }
+    }
+
+    #top-banner{
+        background-color: $primary-color;
+        min-height: 35px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
 </style>
