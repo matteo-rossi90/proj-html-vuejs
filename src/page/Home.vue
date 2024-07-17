fcf2c6cf5afe19db1d742dd3ba5483760a8d0291
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-17 17:58:19 +0200

    feat: card carousel 1

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -7,7 +7,106 @@ export default {
 
 <template>
 <section id="carosello1">
-
+    <div class="container-fluid mt-3 mb-3">
+        <div class="row">
+            <!-- 1 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 2 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 3 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 4 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 5 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 6 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 7 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 8 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 9 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 10 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 11 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>
+            <!-- 12 -->
+            <div class="card" style="width: 25rem;">
+                <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
+                    <p>December 26, 2022 No Comments</p>
+                </div>
+            </div>       
+        </div>
+    </div>
 </section>
 
 <section id="portfolio2">
@@ -26,6 +125,9 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use '../style/generals.scss' as *;
+@use 'src/style/partials/_variables.scss' as *;
+@use 'src/style/partials/_mixins.scss' as *;
 #carosello1{
 
 }
