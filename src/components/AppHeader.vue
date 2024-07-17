7931af57d7c13b4d2b942dfc3cb9c38ac3eb46db
battistaennio <ennio.battista99@gmail.com>
2024-07-17 10:45:36 +0200

    feat: add header section top-banner

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,44 +5,71 @@ export default {
 </script>
 <template>
     <header>
-        <section id="top-banner">
-            <div class="container">
-                <div id="updates">news updates</div>
+        <section id="top-banner" class="container-fluid">
+            <div class="col-1 offset-1" id="updates">
+                news updates
+            </div>
+            <div class="col-8">
+
 
             </div>
-        </section>
-
-        <section>
-            <div class="container">
-    
+            <div class="col-1 row align-items-center justify-content-center" id="social">
+                <a href="#" class="social-i-container">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </a>
+                <a href="#" class="social-i-container">
+                    <i class="fa-brands fa-x-twitter"></i>
+                </a>
+                <a href="#" class="social-i-container">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="#" class="social-i-container">
+                    <i class="fa-brands fa-youtube"></i>
+                </a>
             </div>
+            <div class="offset-1"></div>
         </section>
     </header>
 </template>
 
 <style lang="scss" scoped>
     @use '../style/partials/variables' as *;
+    header{
+        #top-banner{
+            background-color: $primary-color;
+            height: 40px;
+            display: flex;
 
-    .container{
-        margin: 0 10%;
-
-        #updates{
-            text-transform: uppercase;
-            padding: 10px;
+            #updates{
+            height: 100%;
             background-color: #545454;
+            text-align: center;
+            text-transform: uppercase;
             color: white;
+            line-height: 36px;
+            }
+
+            #social{
+                .social-i-container{
+                    background-color: white;
+                    border-radius: 50%;
+                    text-align: center;
+                    width: 27px;
+                    height: 27px;
+                    position: relative;
+                    margin: 0 2px;
+                }
+                i{
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    translate: -50% -50%;
+                    color: $primary-color;
+                    font-size: 15px;
+                }
+            }
+
         }
 
-        #social{
-
-        }
-    }
-
-    #top-banner{
-        background-color: $primary-color;
-        min-height: 35px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
     }
 </style>
