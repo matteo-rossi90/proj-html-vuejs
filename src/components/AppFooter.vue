190a91739ccb6159e8181316132152c75b0394ec
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-17 12:07:03 +0200

    feat: links and subscriprion footer section

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,23 +8,86 @@ export default {
 <template>
     <footer>
         <div class="container ">
-            <div class="row justify-content-center">
-                <div class="col-2">
-                    <h3>GET STARTED </h3>
+            <div class="row justify-content-center ">
+                <!-- footer links section -->
+                <div class="col-2 mt-5 mb-5">
+                    <h5><a href="#">GET STARTED </a></h5>
+                    <ul>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li> 
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="col-2">
-                    <h3>ABOUT </h3>  
+                <div class="col-2 mt-5 mb-5">
+                    <h5><a href="#"> ABOUT </a></h5>  
+                    <ul>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li> 
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="col-2">
-                    <h3>FEATURES </h3>
+                <div class="col-2 mt-5 mb-5">
+                    <h5><a href="#"> FEATURES </a></h5>
+                    <ul>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li> 
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="col-2">
-                    <h3>QUICK LINKS</h3>
+                <div class="col-2 mt-5 mb-5">
+                    <h5><a href="#"> QUICK LINKS</a></h5>
+                    <ul>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li> 
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                        <li>
+                            <a href="#">Resources</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="col-3">
-                    <h3>SUBSCRIPT</h3>
+                <!-- subscription section -->
+                <div class="col-3 d-flex flex-column mt-5 mb-5">
+                    <h5><a href="#">SUBSCRIBE TO NEWSLETTER</a></h5>
+                    <form action="subscription">
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+                        <button type="button" class="btn subs_bt" data-bs-toggle="button">SUBSCRIBE</button>
+                    </form>
                 </div>
             </div>
+            <!-- <hr> -->
         </div>
     </footer>
 </template>
@@ -38,14 +101,37 @@ footer{
  background-color: $primary-color;
  height: 400px;
     .col-2, .col-3{
-        background-color: $secondary-color;
-        height: 100px;
-        border: solid 0.5px black;
-            h3{
-                color: $secondary-color;
+        height: 200px;
+            //remuvin style decoration defoult from li and a tag 
+            li{
+                list-style: none;
+                line-height: 2rem;
+                  
+            }
+            a{
+                text-decoration: none;
+            }
+            // color of links 
+            h5, a{
+                color:$secondary-color;
+            }
+
+            //styling subscription button
+            .subs_bt{
+                width: 100%;
+                background-color: $quaternary-color;
+                color:$secondary-color ;
+            }
+            .subs_bt:hover{
+                background-color: $secondary-color;
+                color: $primary-color;
+
             }
     }
-
+    // hr{
+    //     color:$secondary-color;
+    //     border: solid 1px;
+    // }
 }
 
     
