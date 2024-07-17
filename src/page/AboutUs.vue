0647ca7a4e411d058b363b96ad837b570385247b
Sirlife <simone@freedome.it>
2024-07-17 10:54:24 +0200

    add content

--- src/page/AboutUs.vue
+++ src/page/AboutUs.vue
@@ -6,13 +6,15 @@ export default {
 
 <template>
 <div class="headerAboutUs d-flex justify-content-center align-items-center">About Us</div>
-<div>About Anime Tech</div>
-<h1>Start improving your Bussines Today!</h1>
+
+    <div>About Anime Tech</div>
+    <h1>Start improving your Bussines Today!</h1>
+
 <p>
 Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
 </template>
 
-<style lang="scss">
+<style  lang="scss" scoped>
 .headerAboutUs {
     //background-image: url(../../public/imgs/assets/300x250.webp);
     // debug
