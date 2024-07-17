3dbbe4d443c44526ae5b9c79b7cbf894e328b929
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-17 12:42:51 +0200

    feat: static elements in footer with respective style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,7 +8,7 @@ export default {
 <template>
     <footer>
         <div class="container ">
-            <div class="row justify-content-center ">
+            <div class="row justify-content-center pt-5  ">
                 <!-- footer links section -->
                 <div class="col-2 mt-5 mb-5">
                     <h5><a href="#">GET STARTED </a></h5>
@@ -79,15 +79,25 @@ export default {
                     </ul>
                 </div>
                 <!-- subscription section -->
-                <div class="col-3 d-flex flex-column mt-5 mb-5">
+                <div class="col-3 d-flex flex-column mt-5 mb-5 justify-content-between subscription_cont">
                     <h5><a href="#">SUBSCRIBE TO NEWSLETTER</a></h5>
-                    <form action="subscription">
+                    <form action="subscription" class="d-flex flex-column justify-content-between mb-5">
                         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
                         <button type="button" class="btn subs_bt" data-bs-toggle="button">SUBSCRIBE</button>
                     </form>
                 </div>
             </div>
-            <!-- <hr> -->
+            <hr>
+            <!-- closing logo -->
+            <div class="row d-flex justify-content-between align-items-baseline">
+                <div class="col-3 mt-3 mb-1 ">
+                    <img src="../../public/imgs/assets/anime-whitelogo-300x89.png" alt="">
+                </div>
+                <div class="col-3">
+                    <span><i class="fa-regular fa-copyright"></i> Anime Tech, Theme by HasnainDEV</span>
+                </div>
+                
+            </div>
         </div>
     </footer>
 </template>
@@ -99,23 +109,25 @@ export default {
 
 footer{
  background-color: $primary-color;
- height: 400px;
+ height: 100%;
+    form {
+        gap: 1.2rem;
+    }
     .col-2, .col-3{
         height: 200px;
             //remuvin style decoration defoult from li and a tag 
             li{
                 list-style: none;
                 line-height: 2rem;
-                  
             }
             a{
                 text-decoration: none;
             }
             // color of links 
-            h5, a{
+            h5, a, span{
                 color:$secondary-color;
             }
-
+            
             //styling subscription button
             .subs_bt{
                 width: 100%;
@@ -127,11 +139,14 @@ footer{
                 color: $primary-color;
 
             }
+            img{
+                width: 100%;
+            }
     }
-    // hr{
-    //     color:$secondary-color;
-    //     border: solid 1px;
-    // }
+    hr{
+        color:$secondary-color;
+        border: solid 1px;
+    }
 }
 
     
