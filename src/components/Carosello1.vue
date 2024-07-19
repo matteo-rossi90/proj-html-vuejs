e0e0d2d9aae5c47f87d1800b72539a6bfe939ee8
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-19 15:26:05 +0200

    fix: button over card carosello1

--- src/components/Carosello1.vue
+++ src/components/Carosello1.vue
@@ -10,152 +10,96 @@ export default{
             image:'../../public/imgs/assets/rice-ball.webp',
             title:'Hygenic Recepe To Prepare Rice',
             dateEvent: 'December 26, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Food'
-                }
-                },
+            textLink: ['Food',]
         },
         {   
             id: 2,
             image:'../../public/imgs/assets/meal-time-150x150.webp',
             title:'The Best Time To Have A Meal',
             dateEvent: 'December 26, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Food'
-                }
-                },
-            },
+            textLink: ['Food',]
+        },
         {
             id: 3,
             image:'../../public/imgs/assets/healthy-foods.webp',
             title:'The Best Healthy Foods',
             dateEvent: 'December 26, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Food'
-                }
-                },
+            textLink: ['Food',]
         },
         {
             id: 4,
             image:'../../public/imgs/assets/winter.webp',
             title:'The Best Winter Outfit',
             dateEvent: 'December 26, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Fashion'
-                }
-                },
+            textLink: ['Fashion',]
         },
         {
             id: 5,
             image:'../../public/imgs/assets/photographers-mistakes.webp',
             title:'Begginer Photographer s Mistakes',
             dateEvent: 'December 26, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Fashion'
-                }
-                },
+            textLink: ['Fashion',]
         },
         {
             id: 6,
             image:'../../public/imgs/assets/ideas-anime.webp',
             title:'Live Ideas You Might Be Anime',
             dateEvent: 'December 26, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Cultures',
-                    '2':'Stories'
-                }
-                },
+            textLink: ['Cultures','Stories']
         },
         {
             id: 7,
             image:'../../public/imgs/assets/visit-france.webp',
             title:'Reasons To Visit France',
             dateEvent: 'December 26, 2022 No Comments',
-                link:{
-                textLink: {
-                    '1':'Lifesyle',
-                    '2':'Travel'
-                }
-                },
+            textLink: ['Lifesyle','Travel']
         },
         {
             id: 8,
             image:'../../public/imgs/assets/travel-alone-300x200.webp',
             title:'Traveling Alone Is Awsome',
             dateEvent: 'December 26, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Stories',
-                    '2':'Travel'
-                }
-                },
+            textLink: ['Stories','Travel']
         },
         {
             id: 9,
             image:'../../public/imgs/assets/success-story.webp',
             title:'The Best Success Stories',
             dateEvent: 'December 26, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Cultures',
-                    '2':'Stories'
-                }
-                },
+            textLink: ['Cultures','Stories']
         },
         {
             id: 10,
             image:'../../public/imgs/assets/best-places.webp',
             title:'Place Fora A Road Trip',
             dateEvent: 'December 25, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Lifestyle',
-                    '1':'Stories',
-                    '2':'Travel'
-                }
-                },
+            textLink: ['Lifestyle','Stories', 'Travel']
         },
         {
             id: 11,
             image:'../../public/imgs/assets/music-love.webp',
             title:'Music The Love Of My Life',
             dateEvent: 'December 25, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Cultures',
-                    '2':'Lifestyle'
-                }
-                },
+            textLink: ['Culture', 'Lifestyle']
         },
         {
             id: 12,
             image:'../../public/imgs/assets/anime-fashion.webp',
             title:'Fashion Trend Now A Days',
             dateEvent: 'December 25, 2022 No Comments',
-            link:{
-                textLink: {
-                    '1':'Fashion',
-                    '2':'Stories'
-                }
-                },
+            textLink: ['Fashion', 'Lifestyle']
         },
         ],
         autoplayInterval: null,
         autoplayDelay: 3000 // tempo in millisecondi tra ogni scorrimento automatico
         }
     },
-    mounted() {
-        this.startAutoplay();
-    },
-    beforeDestroy() {
-        this.stopAutoplay();
-    },
+    // mounted() {
+    //     this.startAutoplay();
+    // },
+    // beforeDestroy() {
+    //     this.stopAutoplay();
+    // },
     methods: {
         startAutoplay() {
             this.autoplayInterval = setInterval(() => {
@@ -186,10 +130,9 @@ export default{
                     <h5>{{element.title}}</h5>
                     <p>{{element.dateEvent}}</p>
                     <!-- Link buttons -->
-                <div class="bottoni" v-for="(item, index) in element.link.textLink" :key="index">
-                    <a href="#" class="btn mt-3 position-absolute top-0  bt-style left-bt ">{{item}}</a>
-                    <a href="#" class="btn mt-3 position-absolute top-0  bt-style right-bt">Culture</a>
-                </div>
+                    <div class="bottoni">
+                        <a v-for="(link, index) in element.textLink" :key="index" href="#" class="btn mt-3 position-absolute top-0 bt-style" :style="{ left: index * 7 + 'em' }">{{ link }}</a>
+                    </div>
                 </div>
             </div>
             <!-- cursor prev -->
@@ -220,16 +163,8 @@ export default{
             padding: 0; 
             width: 23.3rem;
         }
-        // costumizzazione i bottoni sopra le immagini 
-        .left-bt {
-            left: 6.5rem;
-        }
-        .right-bt {
-            right: 6.5rem;
-        }
         .bt-style{
-            --bs-btn-padding-x: 0.7rem;
-            --bs-btn-padding-y: 0.1rem;
+            padding: 0.3rem;
             background-color: $secondary-color;
             color: black ;
                 &:hover{
@@ -261,111 +196,3 @@ export default{
     }
 }
 </style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- <div class="container-fluid mt-3 mb-3">
-    <div class="row">
-                <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/rice-ball.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-                <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/meal-time-150x150.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>THE BEST TIME TO HAVE A MEAL</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-                <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/healthy-foods.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>THE BEST HEALTHY FOODS</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-                <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/winter.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>THE BEST WINTER OUTFITS</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-                <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/photographers-mistakes.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-                <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/ideas-anime.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-                <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/visit-france.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-                <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/travel-alone-300x200.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-                <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/success-story.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-        
-        <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/best-places.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-        
-        <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/music-love.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>
-        
-        <div class="card" style="width: 25rem;">
-            <img src="../../public/imgs/assets/anime-fashion.webp" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5>HYGENIC RECEIPE TO PREPARE RICE</h5>
-                <p>December 26, 2022 No Comments</p>
-            </div>
-        </div>       
-    </div>
-</div> -->
